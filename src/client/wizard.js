76adf344137338739048b5b86d6903a0c585ba14
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-07 10:48:01 +0200

    Outline client Wizard

---
+++ src/client/wizard.js
@@ -0,0 +1,36 @@
+const Wizard = ({
+
+}) => {
+  return (
+    <ul>
+      <li>Event Selection
+        <ul>
+          <li>Your Events</li>
+          <li>Shared Events</li>
+        </ul>
+      </li>
+      <li>Your Details</li>
+      <li>Event Details
+        <ul>
+          <li>Description</li>
+          <li>Tags</li>
+          <li>Opening Times</li>
+          <li>Event Contacts</li>
+          <li>Images</li>
+          <li>Booking Info</li>
+          <li>Further Info</li>
+        </ul>
+      </li>
+      <li>Venue Details
+        <ul>
+          <li>Address</li>
+          <li>Location</li>
+          <li>Tags</li>
+          <li>Facilities</li>
+          <li>Directions</li>
+          <li>Further Info</li>
+        </ul>
+      </li>
+    </ul>
+  )
+}
