395f2536cf9e527717ad732419aead0b66e95c5b
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-18 17:53:35 +0100

    Add material-ui and stub

--- src/app/components/AdminApp/index.js
+++ src/app/components/AdminApp/index.js
@@ -1,53 +1,33 @@
 import React from 'react'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-import EventList from 'app/components/EventList'
-import EventAddForm from 'app/components/EventAddForm'
-
-import { useQuery, useMutation } from 'react-apollo-hooks'
-import eventsQuery from 'app/graphql/eventsQuery'
-import eventAddMutation from 'app/graphql/eventAddMutation'
-
-const useEventsQuery = () => {
-  const { loading, error, data: {events = []} = {} } = useQuery(eventsQuery)
-  return { loading, error, events }
-}
-
-const useEventAddMutation = () => {
-  const mutate = useMutation(eventAddMutation, {
-    update: (cache, { data: { eventAdd: event }}) => {
-      const { events } = cache.readQuery({ query: eventsQuery })
-      events.push(event)
-      cache.writeQuery({ query: eventsQuery, data: { events }})
-    },
-  })
-  return event => {
-    mutate({ variables: { event } })
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
   }
-}
-
+}))
 export const AdminApp = ({
+
 }) => {
-  const { loading, error, events } = useEventsQuery()
-  const eventAdd = useEventAddMutation()
+  const classes = useStyles()
 
   return (
-    <section>
-      <h1>Events</h1>
-      <EventList loading={loading} error={error} events={events} />
-      <EventAddForm eventAdd={eventAdd} />
-    </section>
+    <div className={classes.root}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Hello World
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
-export const AdminAppProvider = props => {
-  const client = new ApolloClient()
-  return (
-    <ApolloHooksProvider client={client}>
-      <AdminApp {...props} />
-    </ApolloHooksProvider>
-  )
-}
-
-export default AdminAppProvider
+export default AdminApp
