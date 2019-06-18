7ae3d1fcea311fa0aae6c19560ee0e0255ecb6f9
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-18 17:52:55 +0100

    Render placeholder instead of app on server

--- src/server/httpServer/routes/index.js
+++ src/server/httpServer/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import AdminApp from 'app/components/AdminApp'
 import renderComponentToHtmlResponse from 'server/httpServer/renderComponentToHtmlResponse'
 
+const Placeholder = () => <div>App Loading...</div>
 export const root = (req, res, next) => {
 
   return Promise.resolve()
-    .then(() => renderComponentToHtmlResponse(req)(AdminApp))
+    .then(() => renderComponentToHtmlResponse(req)(Placeholder))
     .then(({ status, payload }) => {
       return res
         .status(status || 200)
