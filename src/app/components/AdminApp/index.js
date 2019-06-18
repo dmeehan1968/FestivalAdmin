fce16b9e797411f040c54f75cd65453138c5f347
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-18 18:01:25 +0100

    Add icons

--- src/app/components/AdminApp/index.js
+++ src/app/components/AdminApp/index.js
@@ -3,10 +3,16 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+  },
+  menuIcon: {
+    marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -21,9 +27,15 @@ export const AdminApp = ({
     <div className={classes.root}>
       <AppBar>
         <Toolbar>
+          <IconButton edge="start" className={classes.menuIcon} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Hello World
+            10 Parishes Festival
           </Typography>
+          <Button color="inherit">
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
