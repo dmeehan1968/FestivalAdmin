9c11f2fd060d431f2beda0331b333feed155f101
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-19 08:18:10 +0100

    Add Helmet, basic dashboard implementation

--- src/app/components/AdminApp/index.js
+++ src/app/components/AdminApp/index.js
@@ -1,36 +1,127 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Styles
+import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
+
+// Core
 import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Divider from '@material-ui/core/Divider'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import Button from '@material-ui/core/Button'
+
+// Icons
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import DashboardIcon from '@material-ui/icons/Dashboard'
 import MenuIcon from '@material-ui/icons/Menu'
+
+import { Helmet } from 'react-helmet'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer+1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  toolbar: {
+    paddingRight: 24,
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
   },
   menuIcon: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-  }
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
 }))
+
 export const AdminApp = ({
 
 }) => {
   const classes = useStyles()
+  const [ isDrawerOpen, setIsDrawerOpen ] = useState(false)
 
   return (
     <div className={classes.root}>
-      <AppBar>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuIcon} color="inherit" aria-label="Menu">
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Helmet>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, isDrawerOpen && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            className={classes.menuIcon}
+            color="inherit"
+            aria-label="Menu"
+            onClick={()=>setIsDrawerOpen(true)}
+            className={clsx(classes.menuButton, isDrawerOpen && classes.menuButtonHidden)}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography component="h1" variant="h6" noWrap className={classes.title}>
             10 Parishes Festival
           </Typography>
           <Button color="inherit">
@@ -38,6 +129,31 @@ export const AdminApp = ({
           </Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !isDrawerOpen && classes.drawerPaperClose)
+        }}
+        open={isDrawerOpen}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={()=>setIsDrawerOpen(false)}>
+            <ChevronLeftIcon/>
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <div>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+          </div>
+        </List>
+        <Divider />
+      </Drawer>
     </div>
   )
 }
--- src/server/components/Html/index.js
+++ src/server/components/Html/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 export const Html = ({
   children,
   styles = [],
   scripts = [],
 }) => {
+  const head = Helmet.renderStatic()
   return (
     <html>
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        {head.meta.toComponent()}
+        {head.title.toComponent()}
+        {head.base.toComponent()}
+        {head.link.toComponent()}
+        {head.script.toComponent()}
+        {head.noscript.toComponent()}
+        {head.style.toComponent()}
         {styles.map((style, key) => {
           if (typeof style === 'string') {
             return <link key={key} type="text/css" rel="stylesheet" href={style} />
