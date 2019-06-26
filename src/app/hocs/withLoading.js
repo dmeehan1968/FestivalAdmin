53b699c066cfb72ef2edf33ba30323fdeac2215b
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-26 11:22:43 +0100

    Remove hocs, not currently needed

--- src/app/hocs/withLoading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const withLoading = Loading => WrappedComponent => ({loading, ...props}) => {
-  return loading && <Loading /> || <WrappedComponent {...props} />
-}
-
-export default withLoading
