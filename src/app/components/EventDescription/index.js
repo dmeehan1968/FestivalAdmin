527262452431fd3e9885cc902b5f6782261a1b50
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-27 12:45:05 +0100

    Remove Typography, redundant

--- src/app/components/EventDescription/index.js
+++ src/app/components/EventDescription/index.js
@@ -9,9 +9,7 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
 
-import ValidatedTextField from 'app/components/ValidatedTextField'
 import Loading from 'app/components/Loading'
 
 import useModelValidations from 'app/hooks/useModelValidations'
