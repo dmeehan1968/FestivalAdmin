b5acbe0c68fa7adcc2e6d0ca7b710456c4a45bed
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-27 18:24:16 +0100

    Refactor Form and Field to encapsulate Formik and Material UI

--- src/app/components/UserProfile/index.js
+++ src/app/components/UserProfile/index.js
@@ -13,7 +13,7 @@ import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
 // Formik
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form as FormikForm, Field as FormikField } from 'formik'
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -25,12 +25,39 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const FormField = ({
+const Form = ({
+  children,
+  classes,
+  ...props,
+}) => {
+  return (
+    <Grid container spacing={3} direction="column" alignItems="center">
+      <Grid item xs={12} md={6}>
+        <Formik {...props}>
+          {args => {
+            return (
+              <FormikForm>
+                <Paper className={classes.paper}>
+                  <Grid container spacing={3}>
+                    {children(args)}
+                  </Grid>
+                </Paper>
+              </FormikForm>
+            )
+          }}
+        </Formik>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+const Field = ({
   xs, sm, md, lg, xl,
   ...props
 }) => {
   return (
-    <Field name={props.name}>
+    <FormikField name={props.name}>
       {({ field, form }) => {
         const key = field.name
         const error = form.touched[key] && form.errors[key]
@@ -54,7 +81,40 @@ const FormField = ({
           </Grid>
         )
       }}
-    </Field>
+    </FormikField>
+  )
+}
+
+const ResetSave = ({
+  classes,
+  canReset,
+  canSubmit,
+}) => {
+  return (
+    <Grid item xs={12}>
+      <Grid container spacing={1} justify="flex-end">
+        <Grid item>
+          <Button
+            type="reset"
+            className={classes.button}
+            disabled={!canReset}
+          >
+            Reset
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={!canSubmit}
+          >
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
@@ -86,65 +146,35 @@ export const UserProfile = ({
   }, [ user ])
 
   return (
-    <Grid container spacing={3} direction="column" alignItems="center">
-      <Grid item xs={12} md={6}>
-        <Formik
-          initialValues={user}
-          onSubmit={handleSubmit}
-          validate={handleValidate}
-        >
-          {({ dirty, isSubmitting, isValid, resetForm }) => {
-            return (
-              <Form>
-                <Paper className={classes.paper}>
-                  <Grid container spacing={3}>
-                    <FormField
-                      xs={12} sm={6}
-                      name="firstName"
-                      label="First Name"
-                    />
-                    <FormField
-                      xs={12} sm={6}
-                      name="lastName"
-                      label="Last Name"
-                    />
-                    <FormField
-                      xs={12}
-                      name="telephone"
-                      label="Telephone"
-                    />
-                    <Grid item xs={12}>
-                      <Grid container spacing={1} justify="flex-end">
-                        <Grid item>
-                          <Button
-                            type="reset"
-                            className={classes.button}
-                            disabled={!dirty}
-                          >
-                            Reset
-                          </Button>
-                        </Grid>
-                        <Grid item>
-                          <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            className={classes.button}
-                            disabled={isSubmitting || !isValid}
-                          >
-                            Save
-                          </Button>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Paper>
-              </Form>
-            )
-          }}
-        </Formik>
-      </Grid>
-    </Grid>
+    <Form
+      classes={classes}
+      initialValues={user}
+      onSubmit={handleSubmit}
+      validate={handleValidate}
+    >
+      {({ dirty, isSubmitting, isValid }) => {
+        return (
+          <>
+            <Field
+              xs={12} sm={6}
+              name="firstName"
+              label="First Name"
+            />
+            <Field
+              xs={12} sm={6}
+              name="lastName"
+              label="Last Name"
+            />
+            <Field
+              xs={12}
+              name="telephone"
+              label="Telephone"
+            />
+            <ResetSave classes={classes} canReset={dirty} canSubmit={!isSubmitting && isValid} />
+          </>
+        )
+      }}
+    </Form>
   )
 }
 
