4fd2cab1ac99fadf880f6a41b6c3aad10f42e095
Dave Meehan <dave_meehan@replicated.co.uk>
2019-06-29 15:01:25 +0100

    Add descriptive error messages

--- src/app/components/Auth/index.js
+++ src/app/components/Auth/index.js
@@ -125,13 +125,15 @@ export const Auth = ({
   const [ credentials, setCredentials ] = useState({ email: '', password: '', confirmPassword: '' })
   const loginSchema = useMemo(() => {
     return yup.object().shape({
-      email: yup.string().email().required(),
+      email: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('Email is required'),
       password: yup
         .string()
-        .required()
-        .min(8)
+        .required('Password is required')
+        .min(8, 'Password should contain at least 8 characters')
         .matches(/[a-z]+/, 'Password must contain at least one lowercase letter')
-        .matches(/[A-Z]+/, 'Password must contain at least one uppercase letter')
         .matches(/[A-Z]+/, 'Password must contain at least one uppercase letter')
         .matches(/[0-9]+/, 'Password must contain at least one number')
         .matches(/\W+/, 'Password must contain at least one non-word character')
