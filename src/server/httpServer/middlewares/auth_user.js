268850147989b1784d995e920eb67a07214fb1bf
Dave Meehan <dave_meehan@replicated.co.uk>
2019-07-11 09:48:03 +0100

    Improve handling of non-authenticated sessions

--- src/server/httpServer/middlewares/auth_user.js
+++ src/server/httpServer/middlewares/auth_user.js
@@ -5,23 +5,26 @@ import fs from 'fs'
 export default app => [
   (req, res, next) => {
     if (req.headers.authorization) {
-      const token = req.headers.authorization.match(/^Bearer: (.+)$/)[1]
-      if (token) {
-        const rsaPrivateKey = fs.readFileSync(path.resolve('.rsa'))
-        jwt.verify(
-          token,
-          rsaPrivateKey,
-          { algorithms: [ 'RS256' ] },
-          (err, user) => {
-            if (err) throw err
-            const { AuthUser } = app.get('models')
-            AuthUser
-            .findByPk(user.id)
-            .then(user => req.user = user)
-            .finally(next)
-          }
-        )
-        return
+      const matches = req.headers.authorization.match(/^Bearer: (.+)$/)
+      if (matches && matches[1]) {
+        const token = matches[1]
+        if (token) {
+          const rsaPrivateKey = fs.readFileSync(path.resolve('.rsa'))
+          jwt.verify(
+            token,
+            rsaPrivateKey,
+            { algorithms: [ 'RS256' ] },
+            (err, user) => {
+              if (err) throw err
+              const { AuthUser } = app.get('models')
+              AuthUser
+              .findByPk(user.id)
+              .then(user => req.user = user)
+              .finally(next)
+            }
+          )
+          return
+        }
       }
     }
     next()
